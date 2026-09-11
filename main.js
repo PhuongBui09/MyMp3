8f7054fd86dca65d4719aa7bc4f2e3d8351993d9
phuongbui <phuongkaratedo113@gmail.com>
2026-09-11 13:17:27 +0700

    .

--- main.js
+++ main.js
@@ -310,10 +310,7 @@ orbitStepNext.addEventListener("click", () => rotateOrbit(1));
 playList.addEventListener(
   "wheel",
   (event) => {
-    if (
-      window.matchMedia("(min-width: 881px)").matches &&
-      !document.body.classList.contains("two-panel-mode")
-    ) {
+    if (window.matchMedia("(min-width: 881px)").matches) {
       event.preventDefault();
       rotateOrbit(event.deltaY > 0 ? -1 : 1);
     }
